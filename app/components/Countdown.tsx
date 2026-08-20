09973f977e42f36b2cad7b5d5a4803bcabe2e36c
shadiltn-boop <shadiltn@gmail.com>
2026-08-21 00:17:37 +0530

    Update Countdown.tsx

--- app/components/Countdown.tsx
+++ app/components/Countdown.tsx
@@ -1,6 +1,6 @@
 import { memo, useEffect, useState } from "react";
 
-const EVENT_DATE = "Juliy 30, 2022 08:00:00";
+const EVENT_DATE = "March 17, 2027 12:00:00";
 const COUNTDOWN_DATE = new Date(EVENT_DATE).getTime();
 
 interface COUNTDOWNTYPE {
